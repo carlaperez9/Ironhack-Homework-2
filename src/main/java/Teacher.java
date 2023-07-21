5117142429284d7a087029e01eece5b572790db3
Orlando-Mediro <oj99medina@gmail.com>
2023-07-21 15:26:29 -0400

    created the teacher class

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Teacher {
+    // Testing out to implement in the auto-generated ID
+    private static final AtomicInteger idCounter = new AtomicInteger(1000);
+    // properties
+    private String teacherId;
+    private String name;
+    private double salary;
+
+    // constructor
+    public Teacher(String name, double salary) {
+        setTeacherId();
+        setName(name);
+        setSalary(salary);
+    }
+
+    // setters and getters
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId() {
+        this.teacherId = String.valueOf(idCounter.getAndIncrement());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
